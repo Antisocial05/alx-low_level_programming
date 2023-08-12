65ceec0bd0b82258417e98e9e61bd1a25a196d85
Stephen Omoko <jacksonwilson010@gmail.com>
2023-08-12 13:45:52 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,14 +6,14 @@
 */
 int main(void)
 {
-	int i = '0';
+	int i;
 
-	while (i <= '9')
+	for (i = 48; i < 58 ; i++)
 	{
 		putchar(i);
-		putchar('\n');
-		i++;
 	}
+	putchar('\n');
 
 	return (0);
+
 }
