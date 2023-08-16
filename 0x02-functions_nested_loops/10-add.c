589ce2a7af0b7d122dde19cc86610a1ef058823d
Stephen Omoko <jacksonwilson010@gmail.com>
2023-08-16 06:01:47 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add - adding two integers
+* @a: first parameter
+* @b: second parameter
+* Return: result
+*/
+int add(int a, int b)
+{
+	int result;
+
+	result - a + b;
+		return (result);
+}
