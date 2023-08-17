652dbacc3319d36059c5f80fae7220650967d99c
Stephen Omoko <jacksonwilson010@gmail.com>
2023-08-17 16:51:25 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -8,29 +8,10 @@
 */
 int mul(int a, int b)
 {
-	int result = 0;
+	int sum;
 
-	if (a < 0 && b < 0)
-	{
-		a = -a;
-		b = -b;
-	}
-	else if (a < 0)
-	{
-		a = -a;
-		result = -1;
-	}
-	else if (b < 0)
-	{
-		b = -b;
-		result = -1;
-	}
-	while (b > 0)
-	{
-		result += a;
-		b--;
-	}
-	return (0);
+		sum = a * b;
+	return (sum);
 }
 
 
