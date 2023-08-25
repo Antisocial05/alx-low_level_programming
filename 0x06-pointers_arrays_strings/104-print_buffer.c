4c56a0ea123aefb30b117573410682ab2417d56a
Stephen Omoko <jacksonwilson010@gmail.com>
2023-08-24 17:29:40 -0700

    104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* print_hex - prints hex
+* @str: argument
+* Return: void
+*/
+
+void print_hex(const char *str)
+{
+	unsigned char *p = (unsigned char *)str;
+	size_t i, len;
+
+	len = strlen(str);
+
+	for (i = 0; i < len; i++)
+	{
+		printf("%02x ", p[i]);
+	}
+	printf("\n");
+}
+/**
+* main - main function
+* Return: always 0
+*/
+int main(void)
+{
+	const char *str = "This is a string!";
+
+	printf("%s\n", str);
+	printf("---------------------------------\n");
+	print_hex(str);
+
+	return (0);
+}
