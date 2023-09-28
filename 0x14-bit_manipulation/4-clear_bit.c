b95edcad20b452499480211ed0963187509d0acd
Stephen Omoko <jacksonwilson010@gmail.com>
2023-09-28 16:49:52 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,9 +8,13 @@
 */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8)
+	unsigned long int mask;
+
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	*n &= -(1UL << index);
+	mask = 1UL << index;
+	*n = *n & ~mask;
+
 	return (1);
 }
