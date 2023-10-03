e26d851338b7bc48492374b08f985936c39c604e
Stephen Omoko <jacksonwilson010@gmail.com>
2023-10-02 20:06:42 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
 #include <sys/stat.h>
+#include <string.h>
+
 /**
-* create_file - creates a file
-* @filename: name of file to be created
-* @text_content: content of the file to be created
-* Return: always an integer
-*/
+ * create_file - Creates a file and writes text content to it
+ * @filename: Name of the file to create
+ * @text_content: NULL-terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
 int create_file(const char *filename, char *text_content)
 {
-	FILE *filepointer = NULL;
+	int fd, write_res;
+	mode_t permissions = S_IRUSR | S_IWUSR;
 
-	filepointer = fopen(filename, "w");
-
-	if (filepointer == NULL || filename == NULL)
-	{
+	if (filename == NULL)
 		return (-1);
-	}
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, permissions);
+	if (fd == -1)
+		return (-1);
+
 	if (text_content != NULL)
 	{
-		if (fputs(text_content, filepointer) == EOF)
+		write_res = write(fd, text_content, strlen(text_content));
+		if (write_res == -1)
 		{
-			fclose(filepointer);
+			close(fd);
 			return (-1);
 		}
 	}
-	fclose(filepointer);
-	if (chmod(filename, S_IRUSR | S_IWUSR) != 0)
-		return (-1);
 
+	close(fd);
 	return (1);
 }
